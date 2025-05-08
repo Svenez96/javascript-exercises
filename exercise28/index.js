25c72fd90c30a122566555bd6c30ff6e9f44c631
Svenez96 <mazzadomenico96@yahoo.com>
2025-05-08 10:15:49 +0200

    forEach & map

--- exercise28/index.js
+++ exercise28/index.js
@@ -1,10 +1,8 @@
 const numbers = [1, 2, 3, 4, 5]
 
-numbers.forEach((num)=>{
-   console.log(num * 2);
-});
+numbers.forEach((num) => console.log(num * 2));
 
-const squaredNumbers = numbers.map((num)=>{
+const squaredNumbers = numbers.map((num) => { 
    return(num ** 2);
 });
 
