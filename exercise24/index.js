365c69629a9158bd51513ffe9517702093965ef3
Svenez96 <mazzadomenico96@yahoo.com>
2025-05-08 10:53:37 +0200

    created a counter

--- exercise24/index.js
+++ exercise24/index.js
@@ -1,4 +1,22 @@
-function createCounter() {
-   let increment 
+function createCounter () {
+   let count = 6;
+
+   return {
+      increment: function () {
+         count += 1;
+         return count;
+      },
+
+      decrement: function () {
+         count -= 1;
+         return count;
+      }
+   }
    
 }
+
+const counter = createCounter ()
+
+console.log(counter.increment())
+console.log(counter.decrement())
+
