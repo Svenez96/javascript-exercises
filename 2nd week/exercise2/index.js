8ed6c35d236db267bf602ab5cf6a757c72cb7143
Svenez96 <mazzadomenico96@yahoo.com>
2025-05-12 20:55:58 +0200

    added methods and properties

--- 2nd week/exercise2/index.js
+++ 2nd week/exercise2/index.js
@@ -1,39 +1,37 @@
-class Automobile {
-    constructor(marca, modello, anno) {
+class Automobile{
+  marca = "";
+  modello = "";
+  anno = 0;
+
+    constructor(marca, modello, anno){
       this.marca = marca;
       this.modello = modello;
       this.anno = anno;
     }
   }
-  
- class Chilometraggio extends Automobile {
-  chilometraggio = [];
 
-  constructor(marca, modello, anno, chilometraggio) {
+class autoInfoKm extends Automobile{
+  chilometraggio = 0;
+
+  constructor(marca, modello, anno, chilometraggio){
     super(marca, modello, anno);
     this.chilometraggio = chilometraggio;
   }
- }
 
- descrizione() {
-  return `Automobile: ${this.marca} ${this.modello}, anno ${this.anno}`;
+  aggiungiChilometri(km){
+    if (km > 0) {
+      this.chilometraggio += km;
+    } else {
+      console.log("inserire un valore d'incremento");
+    }
+   } 
+  
+  mostraChilometraggio(){
+   return (`Questa è una ${this.marca} ${this.modello} del ${this.anno}. Chilometraggio attuale: ${this.chilometraggio} km`)
+   }
 }
-
-aggiungiChilometri(km) {
-  if (km > 0) {
-    this.chilometraggio += km;
-  } else {
-    console.log("Il numero di chilometri deve essere positivo.");
-  }
-}
-
-mostraChilometraggio() {
-  return `Chilometraggio attuale: ${this.chilometraggio} km`;
-}
-
-
-// Esempio di utilizzo
-const miaAuto = new Automobile('Toyota', 'Corolla', 2020);
-console.log(miaAuto.descrizione());                      // Output: Automobile: Toyota Corolla, anno 2020
-miaAuto.aggiungiChilometri(150);
-console.log(miaAuto.mostraChilometraggio());    
+  
+  const subaruBaracca = new autoInfoKm("Subaru", "Baracca", 1999, 0);
+  subaruBaracca.aggiungiChilometri(0)
+  subaruBaracca.mostraChilometraggio()
+  console.log(subaruBaracca.mostraChilometraggio());
