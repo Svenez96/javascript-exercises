a2558671ccd4b483ed7c3d7a9153c6da1747b42f
Svenez96 <mazzadomenico96@yahoo.com>
2025-05-14 09:58:07 +0200

    modified mostraContatore

---
+++ 2nd week/exercise10/index.js
@@ -0,0 +1,103 @@
+class Automobile {
+  marca = "";
+  modello = "";
+  anno = 0;
+  chilometraggio = 0;
+
+   //Contatore
+   #contatoreChiamate = 0;
+
+  constructor(marca, modello, anno, chilometraggio){
+    this.marca = marca;
+    this.modello = modello;
+    this.anno = anno;
+    this.chilometraggio = chilometraggio;
+  }
+
+  description(){
+    return `Questa è una ${this.marca} ${this.modello} del ${this.anno}.`;
+  }
+
+  //incremento del contatore
+  #incrementoContatore(){
+    this.#contatoreChiamate++;
+  }
+
+  aggiungiChilometri(km){
+    //aggiunto il contatore chiamate
+    this.#incrementoContatore();
+    if (km > 0) {
+      this.chilometraggio += km;
+    } else {
+      console.log("inserire un valore d'incremento");
+    }
+  }
+
+  mostraContatore(){
+    return `Il metodo aggiungiChilometri è stato chiamato ${this.#contatoreChiamate} volte.`
+  }
+
+  mostraChilometraggio(){
+  return `Chilometraggio attuale: ${this.chilometraggio} km`;
+  }
+
+  #calcolaEtà(){
+    const annoCorrente = new Date().getFullYear()
+    return annoCorrente - this.anno;
+}
+
+  mostraEtà(){
+    return `Età dell'automobile: ${this.#calcolaEtà()} anni`;
+  }
+
+  _controllaChilometri(){
+    if (this.chilometraggio > 100000) {
+      return "Attenzione: chilometraggio elevato!";
+    }
+  }
+
+  static confrontaChilometri(auto1, auto2){
+    if (auto1.chilometraggio > auto2.chilometraggio){
+      return `${auto1.marca} ${auto1.modello} ha più chilometri.`;
+    } else if (auto2.chilometraggio > auto1.chilometraggio) {
+      return `${auto2.marca} ${auto2.modello} ha più chilometri.`;
+    } else {
+      return "Le automobili hanno gli stessi chilometri"
+    }  
+  }
+
+}
+
+Automobile.prototype.saluta = function(){
+  return `Ciao utente, questa è una ${this.marca} ${this.modello}`;
+}
+
+//sottoclasse elettrica 
+class Elettrica extends Automobile{
+  autonomia = 0;
+  constructor(marca, modello, anno, chilometraggio, autonomia){
+    super(marca, modello, anno, chilometraggio);
+    this.autonomia = autonomia;
+  }
+
+  description(){
+    return `${super.description()}. Autonomia: ${this.autonomia} km`;
+    }
+
+  ricaricaChilometri(km){
+    if (km > 0) {
+      this.autonomia += km;
+    } else {
+      console.log("Batteria scarica, ricaricare.");
+    }
+  }
+}
+
+
+const subaruBaracca = new Automobile("Subaru", "Baracca", 1999, 200000);
+const subaruBaraccaElettrica = new Elettrica("Subaru", "Baracca", 1999, 20000, 250);
+subaruBaracca.aggiungiChilometri(200);
+subaruBaracca.aggiungiChilometri(300);
+subaruBaracca.aggiungiChilometri(400);
+console.log(subaruBaracca.mostraContatore());
+
