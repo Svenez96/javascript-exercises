eeea0ceb3e28077bf5c945aa55e905672ce0d5ea
Svenez96 <mazzadomenico96@yahoo.com>
2025-05-20 08:47:37 +0200

    exercise completed

---
+++ 3rd week/exercise10/index.js
@@ -0,0 +1,8 @@
+let numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const contenitore = [...numeri];
+
+numeri = [1, 2, 3, 6, 7, 8];
+
+console.log(numeri);
+console.log(contenitore);
---
+++ 3rd week/exercise8/index.js
@@ -0,0 +1,5 @@
+const numeriPari = [2, 4, 6, 8, 10] 
+const numeriDispari = [1, 3, 5, 7, 9]
+const numeri = [...numeriPari, ...numeriDispari];
+
+console.log(numeri);
