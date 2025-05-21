23cce075e7c3d39323740d43a340f93c3d379908
Svenez96 <mazzadomenico96@yahoo.com>
2025-05-21 10:25:29 +0200

    Utilizzare console.log

---
+++ 3rd week/exercise19/index.js
@@ -0,0 +1,17 @@
+const numero = 2;
+
+const messaggio = "Ciao ragazzi ciao a tutti sono zeb89";
+
+const frutta = ["pera", "mela", "banana", "arancia", "limone"];
+
+const canzone = {
+    titolo: "Wasted Years",
+    artista: "Iron Maiden",
+    genere: "Heavy metal, metal",
+    anno: 1986
+};
+
+console.log(numero);
+console.log(messaggio);
+console.log(frutta);
+console.log(canzone);
