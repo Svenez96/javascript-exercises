d10689813b4c749da632b3d85b5125199d8974f8
Svenez96 <mazzadomenico96@yahoo.com>
2025-05-21 17:34:21 +0200

    Utilizzare setTimeout

---
+++ 3rd week/exercise24/index.js
@@ -0,0 +1,14 @@
+console.table(["Centauromon", "Garurumon", "Kabuterimon", "Metalgreymon", "Sukamon", "Monzaemon"]);
+
+console.log("Lvl 0: Botamon");
+console.group();
+console.log("Lvl 1: Koromon");
+console.group();
+console.log("Lvl 2: Agumon");
+console.group();
+console.log("Lvl 3: Greymon");
+console.group();
+console.log("Lvl 4: Metalgreymon");
+console.group();
+console.log("Lvl 5: Wargreymon");
+console.groupEnd();
