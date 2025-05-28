f696200119fbd7d0f5178d8c3acda4524c668603
Svenez96 <mazzadomenico96@yahoo.com>
2025-05-28 11:44:46 +0200

    catena di promesse con gestione degli errori

--- 4th week/exercise23/index.js
+++
@@ -1,7 +0,0 @@
-function mostraMessaggio(){
-    console.log("Sono passati 2 secondi");
-};
-
-setInterval(mostraMessaggio, 2000);
-
-console.log(mostraMessaggio);
--- 4th week/exercise9/index.js
+++ 4th week/exercise9/index.js
@@ -1,13 +1,23 @@
-const videogame = {
-    type:"RPG",
-    platform:"Playstation",
-    pegi:16
-};
+function errorsCheck() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(Math.floor(Math.random() * 100)); 
+      }, 1000);
+    });
+  }
+  
+  errorsCheck()
+    .then((num) => {
+        if (num > 0 && num <= 50){
+            return num;
+        } else {
+            throw new Error("The number must be lower than 50")
+        }
+    })
+   
+    .then((result) => {
+      console.log("The number is:", result);
+    })
 
-const videogameInfo = {
-    ...videogame,
-    price:59.99
-};
-
-console.log(videogame);
-console.log(videogameInfo);
+    .catch((error) => console.error (error.message))
+  
