c18dd572cd49bff88066869732f64333eeb1b68b
Svenez96 <mazzadomenico96@yahoo.com>
2025-06-05 15:12:23 +0200

    finished exercise

--- 5th week/exercise1/index.js
+++ 5th week/exercise1/index.js
@@ -1,25 +1,10 @@
 function setCookie(name, value, days) {
-  const d = new Date();
-  d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-  const expires = "expires=" + d.toUTCString();
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
   const cookieString = name + "=" + value + ";" + expires + ";path=/";
   document.cookie = cookieString;
   console.log("Cookie creato:", cookieString);
-}
-
-function getCookie(name) {
-  const nameEQ = name + "=";
-  const cookiesArray = document.cookie.split(';');
-  for (let i = 0; i < cookiesArray.length; i++) {
-    let c = cookiesArray[i].trim();
-    if (c.indexOf(nameEQ) === 0) {
-      const value = c.substring(nameEQ.length);
-      console.log(`Cookie trovato: ${name} = ${value}`);
-      return value;
-    }
-  }
-  console.log(`Cookie ${name} non trovato.`);
-  return null;
 }
 
 
@@ -29,5 +14,4 @@ function deleteCookie(name) {
 }
 
 setCookie("nome utente", "Giovanni Belardi", 7)
-getCookie("nome utente")
 deleteCookie("nome utente")
