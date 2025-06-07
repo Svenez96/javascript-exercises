cf313e7ca6649d50966e45df8af3ed0903688434
Svenez96 <mazzadomenico96@yahoo.com>
2025-06-07 17:30:01 +0200

    Gestire i cookies

--- 5th week/exercise1/index.js
+++ 5th week/exercise1/index.js
@@ -7,11 +7,23 @@ function setCookie(name, value, days) {
   console.log("Cookie creato:", cookieString);
 }
 
+function getCookie(name){
+  let cookies = document.cookie.split(';')
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i].trim();
+    if(cookie.startsWith(name + "=")){
+      return cookie.substring(name.length + 1)
+    }  
+  }
+  return null
+}
 
 function deleteCookie(name) {
   document.cookie = name + "=; expires=Thu, 04 Jun 2025 00:00:00 UTC; path=/;";
   console.log(`Cookie cancellato: ${name}`);
 }
 
-setCookie("nome utente", "Giovanni Belardi", 7)
-deleteCookie("nome utente")
+setCookie("user", "Giovanni Belardi", 7)
+console.log(getCookie("user"))
+deleteCookie("user")
+console.log(getCookie("user"))
